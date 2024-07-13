402775858174b257023313a954f930127ae32d79
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2024-07-13 23:21:43 +0300

    web/driver.scm: Add the module license nad commentary

--- web/driver.scm
+++ web/driver.scm
@@ -1,3 +1,28 @@
+;;; driver.scm -- Selenium WebDriver implementation.
+
+;; Copyright (C) 2019-2024 Michal Herko <michal.herko@disroot.org>
+;; Copyright (C) 2024 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, version 3.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; The main module of Selenium WebDriver for GNU Guile.
+
+
+;;; Code:
+
 (define-module (web driver))
 
 (use-modules
@@ -723,3 +748,4 @@ localhost:8080."
         (apply perform driver (cdr actions))))))
   (if (not (null? actions)) (perform-actions '() '() actions)))
 
+;;; driver.scm ends here.
