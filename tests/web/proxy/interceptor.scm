2ce96f27f3689a5ba134f3cdc8a0cf6bf7ff3733
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2024-07-24 22:38:07 +0300

    tests/web/proxy/interceptor: New test suite
    * tests/web/proxy/interceptor.scm: New test suite.
    * tests/Makefile.am (SCM_TESTS): Register "tests/web/proxy/interceptor.scm".

---
+++ tests/web/proxy/interceptor.scm
@@ -0,0 +1,33 @@
+(use-modules (srfi srfi-64)
+             (srfi srfi-26)
+             (oop goops)
+             (web proxy connection)
+             (web proxy interceptor))
+
+
+(define %test-name "web/proxy/interceptor")
+
+
+(test-begin %test-name)
+
+(define %certificate
+  (format #f "~a/cert/cert.pem" (getenv "abs_top_srcdir")))
+(define %key
+  (format #f "~a/cert/key.pem" (getenv "abs_top_srcdir")))
+
+(test-assert "make <proxy-interceptor>"
+  (make <proxy-interceptor>
+    #:x509-certificate-file %certificate
+    #:x509-private-key-file %key))
+
+(test-assert "display"
+  (display (make <proxy-interceptor>
+             #:x509-certificate-file %certificate
+             #:x509-private-key-file %key)))
+
+
+(define exit-status (test-runner-fail-count (test-runner-current)))
+
+(test-end %test-name)
+
+(exit (zero? exit-status))
