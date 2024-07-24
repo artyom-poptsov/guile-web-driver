3ad80d51a2c94bddcc5e452c694fc8fa47d03a65
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2024-07-24 15:05:06 +0300

    tests/web/proxy/connection.scm: New test suite
    * tests/web/proxy/connection.scm: New test suite.
    * tests/Makefile.am (SCM_TESTS): Register "tests/web/proxy/connection.scm".

---
+++ tests/web/proxy/connection.scm
@@ -0,0 +1,29 @@
+(use-modules (srfi srfi-64)
+             (srfi srfi-26)
+             (oop goops)
+             (web proxy connection))
+
+
+(define %test-name "web/proxy/connection")
+
+
+(test-begin %test-name)
+
+(test-assert "make <proxy-connection>"
+  (make <proxy-connection>))
+
+(test-assert "display"
+  (display (make <proxy-connection>)))
+
+(test-equal "proxy-connection-client-port"
+  (current-error-port)
+  (proxy-connection-client-port
+   (make <proxy-connection>
+     #:client `(,(current-error-port) . #()))))
+
+
+(define exit-status (test-runner-fail-count (test-runner-current)))
+
+(test-end %test-name)
+
+(exit (zero? exit-status))
