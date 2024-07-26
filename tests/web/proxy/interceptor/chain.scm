632b826f8fac2a931bbd9780ae39d3f1ae8be949
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2024-07-26 13:33:44 +0300

    tests/web/proxy/interceptor/chain.scm: New test suite
    * tests/web/proxy/interceptor/chain.scm: New test suite.
    * tests/Makefile.am (SCM_TESTS): Register "web/proxy/interceptor/chain.scm".

---
+++ tests/web/proxy/interceptor/chain.scm
@@ -0,0 +1,49 @@
+(use-modules (srfi srfi-64)
+             (srfi srfi-26)
+             (oop goops)
+             (web proxy interceptor chain))
+
+
+(define %test-name "web/proxy/interceptor/chain.scm")
+
+
+(test-begin %test-name)
+
+(define %rule-1 '(request method dump stdout))
+(define %rule-2 '(response method dump stdout))
+(define %chain-1 `(,%rule-1 ,%rule-2))
+
+(test-equal "rule:type"
+  'request
+  (rule:type %rule-1))
+
+(test-equal "rule:field"
+  'method
+  (rule:field %rule-1))
+
+(test-equal "rule:action"
+  'dump
+  (rule:action %rule-1))
+
+(test-equal "rule:parameters"
+  'stdout
+  (rule:parameters %rule-1))
+
+(test-equal "chain-select"
+  `(,%rule-1)
+  (chain-select %chain-1 'request))
+
+(test-equal "chain-run: dump"
+  "\"test\"\n"
+  (with-output-to-string
+    (lambda ()
+      (chain-run (chain-select %chain-1 'request)
+                 'method
+                 "test"))))
+
+
+(define exit-status (test-runner-fail-count (test-runner-current)))
+
+(test-end %test-name)
+
+(exit (zero? exit-status))
