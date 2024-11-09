1fc4bd2b4e7632dddec9ce47708a05a39524220d
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2024-11-09 18:32:35 +0300

    modules/web/driver/request.scm: Add module commentary

--- modules/web/driver/request.scm
+++ modules/web/driver/request.scm
@@ -1,3 +1,30 @@
+;;; request.scm -- Guile-WebDriver-NG HTTP requests.
+
+;; Copyright (C) 2019-2024 Michal Herko <michal.herko@disroot.org>
+;; Copyright (C) 2024 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module defines HTTP request-related procedures for working with
+;; Selenium API.
+
+
+;;; Code:
+
 (define-module (web driver request)
   #:use-module (json)
   #:use-module (web client)
@@ -8,6 +35,7 @@
   #:use-module (web driver common)
   #:export (request))
 
+
 (define (request method uri body-scm)
   (log-debug "request: method: ~a uri: ~a"
              method
